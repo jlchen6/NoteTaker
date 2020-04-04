399a5ece354877074f65c96ced2b6e959ef98786
Julia <jlchen6@wisc.edu>
2020-04-03 23:16:26 -0500

    Added comments and deleted extraneous clear function that wasn't being called anywhere.

--- db/notes.js
+++ db/notes.js
@@ -1,16 +1,24 @@
+// import required libraries
 const fs = require("fs");
 const path = require("path");
 
+// data contains the json stored in the db.json file
 var data = require("../db/db.json");
+// Store the path to the json file in a variable for easy use later
 var dbPath = path.join(__dirname, '../db/db.json');
 
+// Function that just returns the json
 function readNotes() {
     return data;
 }
 
+// Function that saves the given note to the json file
 function saveNotes(note) {
+    // Set the note's id to the length of the array
     note.id = data.length;
+    // push the note onto the current json array
     data.push(note);
+    // write the updated array to the json file
     fs.writeFile(dbPath, JSON.stringify(data), error => {
         if(error){
             throw error;
@@ -19,11 +27,15 @@ function saveNotes(note) {
 
 }
 
+// Function that deletes the note with the given id from the json file
 function deleteNote(id) {
+    // remove the note at index: id from the json array
     data.splice(id, 1);
+    // Update the ids for every other note to match their updated position in the array
     for (let i = 0; i < data.length; i++) {
         data[i].id = i;
     }
+    // Write the updated array to the json file
     fs.writeFile(dbPath, JSON.stringify(data), error => {
         if(error){
             throw error;
@@ -31,18 +43,9 @@ function deleteNote(id) {
     })
 }
 
-function clearNotes() {
-    var clearedNotes = [];
-    fs.writeFile(dbPath, JSON.stringify(clearedNotes), error => {
-        if(error){
-            throw error;
-        }
-    })
-}
-
+// Put all the functions to be exported into the module.exports.
 module.exports = {
     readNotes,
     saveNotes,
-    clearNotes,
     deleteNote
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,29 @@
-
+// Import the functions that directly edit the json file
 var notes = require("../db/notes");
 
-
+// The module that will be exported. In this case, we are exporting a function so that we can pass in the app object to use further down in the code.
 module.exports = function(app) {
 
+  // create an API get method that returns the data stored in the json file
   app.get("/api/notes", function(req, res) {
     // Returns the data from the db.json file
     res.json(notes.readNotes());
   });
 
+  // Create an API post method that saves a note to the json file
   app.post("/api/notes", function(req, res) {
-    var newNote = req.body;
-    notes.saveNotes(newNote);
+    // Save the submitted note to the json file
+    notes.saveNotes(req.body);
     res.json(true);
   });
 
+  // Create an API delete method that deletes the note with the given id from the json file
   app.delete("/api/notes/:id", function(req, res) {
-    // TODO: delete notes from json
+    // Grab the id parameter from the route
     var id = req.params.id;
+    // Delete the note with the given id from the json file
     notes.deleteNote(id);
     res.json(true);
   })
 
-  app.post("/api/clear", function(req, res) {
-    // Empty out the json
-    notes.clearNotes();
-    res.json(true);
-  });
 };
